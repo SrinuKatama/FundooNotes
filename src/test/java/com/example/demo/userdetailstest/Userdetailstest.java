605f9b1b8d9192aa53c4f730f7504ad4bef421d9
SrinuKatama <59701542+SrinuKatama@users.noreply.github.com>
2020-04-13 12:30:31 +0530

    [srinu]testcases

--- src/test/java/com/example/demo/userdetailstest/Userdetailstest.java
+++ src/test/java/com/example/demo/userdetailstest/Userdetailstest.java
@@ -1,9 +1,43 @@
 package com.example.demo.userdetailstest;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.bridgelabz.fundoonotes.model.UserDetails;
+import com.bridgelabz.fundoonotes.repository.UserRepository;
 import com.bridgelabz.fundoonotes.serviceImplementation.UserService;
 
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class Userdetailstest 
 {
-	UserService userserve=new UserService();
+	
+	@MockBean
+	UserRepository userrepo;
+	
+	@Autowired
+	UserService userserve;
+	
+	
+	@Test
+	public void getUserstest()
+	{
+		when(userrepo.getUserList()).thenReturn(Stream.of(new UserDetails(1,"srinu","b","srinu@123","123","95425")).
+				collect(Collectors.toList()));
+		assertEquals(1,userserve.getAllusers("admin").size());
+		
+	}
 
 }
